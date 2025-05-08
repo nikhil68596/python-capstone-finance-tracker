efeaf4367b5af4e5e1872161ef74187d90d990cd
Nikhil Saireddy Munagala <nikhilsai.munagala@gmail.com>
2025-05-08 14:55:59 -0400

    Finished the capstone project for python.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Finance Tracker
+
+Take control of your finances using my Python-based Finance Tracker! Whether you're managing your monthly budget through tracking expenses, or aggregating expense costs by category, this tool provides you a platform to help you make smarter decisions on your money. 
+
+## How to Run the Script
+
+1. Clone the repository:
+    git clone https://github.com/your-username/python-capstone-finance-tracker.git
+    cd python-capstone-finance-tracker
+2. Make sure that you have Python 3 installed on your system.
+3. Install any required dependencies:  pip install -r requirements.txt
+4. Run the program: python finance_tracker.py
+
+## Python Concepts Used
+
+- input() for effectively reading user input
+- Exception handling for strict type errors and malformed inputs
+- Dictionaries and list to manage a list of expenses per category
+    - Tuple to represent an expense
+- Functions for modular and reusable code
+- For loops for iterating through the expenses
+- While loops for repeatedly entering the user input if continuously invalid
--- finance_tracker.py
+++ finance_tracker.py
@@ -1,42 +1,72 @@
 # Capstone Project: Personal Finance Tracker
 print("Welcome to the Personal Finance Tracker!\n")
 
+expenses = {}
+
+#Create a method that adds an expense
+def add_expense(descp, category, amount):
+    if category not in expenses:
+        expenses[category] = [(descp, amount)]
+    else:
+        expenses[category].append((descp, float(amount)))
+
+def view_all_expenses(data):
+    if not data:
+        print("No expenses recorded yet.")
+        return
+    for category, items in data.items():
+        print(f"Category: {category}")
+        for descp, amount in items:
+            print(f"  - {descp}: ${amount:.2f}")
+
+def view_category_expenses(data):
+    for category in data:
+        total_amount = sum([expense[1] for expense in data[category]])
+        print(f"{category}: ${total_amount}")
+
 while True:
-    option = int(input("What would you like to do? \n1. Add Expense \n2. View All Expenses \n3. View Summary \n5. Exit >"))
-    print("\n")
-    if option == 5: #Exit
+    def get_option():
+        try:
+            option = int(input("What would you like to do? \n1. Add Expense \n2. View All Expenses \n3. View Summary \n4. Exit \nChoose an option: "))
+            print("\n")
+            return option
+        except ValueError: #If you didn't enter an integer-based option.
+            print("\n")
+            return -1
+    
+    option = get_option()
+    if option == 4: #Exit
         print("Goodbye!")
         break
-    elif not (option >= 1 and option <= 5): #Invalid option
-        print("Invalid option. Please try again.")
-        print("\n")
-        continue
-    
-    #Keep entering the numbers till it's valid.
-    while True:
-        try:  
-            first_num = int(input("Enter the first number: "))
-            break
-        except ValueError:
-            print("Invalid input! Please enter a valid number.")
-            
-    while True:
-        try:  
-            second_num = int(input("Enter the second number: "))
-            break
-        except ValueError:
-            print("Invalid input! Please enter a valid number.")
-            
+    elif not (option >= 1 and option <= 4): #Invalid option
+        if option == -1:
+            print("An option strictly has to be a number. Please try again.")
+            print("\n")
+            continue
+        else:
+            print("Invalid option. Please try again.")
+            print("\n")
+            continue
+         
     #Performing the menu options.
-    if option == 1: #Addition
-        print(f"The sum of {first_num} and {second_num} is {first_num + second_num:.2f}.")
-    elif option == 2: #Subtraction
-        print(f"The difference between {first_num} and {second_num} is {first_num - second_num:.2f}.")
-    elif option == 3: #Multiplication
-        print(f"The product of {first_num} and {second_num} is {first_num * second_num:.2f}.")
-    elif option == 4: #Division
-        try:
-            print(f"{first_num} divided by {second_num} is {first_num / second_num:.2f}.")
-        except ZeroDivisionError:
-            print("Oops! Division by zero is not allowed.")
+    if option == 1: #Add expense
+        #Ask user to enter description, category, and amount
+        expense_descp = input("Enter expense description:")
+        category = input("Enter category:")
+        while True:
+            try:
+                amount = float(input("Enter amount:"))
+                if amount <= 0:
+                    print("Invaid amount. Please enter an amount greater than 0.")
+                    continue
+                break  # Exit loop if amount is valid
+            except ValueError:
+                print("Invalid amount. Please enter a numeric value (e.g., 12.50)")
+        add_expense(expense_descp, category, amount)
+        print("Expense added successfully.")
+    elif option == 2: #View all expenses
+        # Create a function that views all expenses and call it here.
+        view_all_expenses(expenses)
+    elif option == 3: #View summary
+        view_category_expenses(expenses)
     print("\n")
